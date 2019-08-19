df1268d41e6050f9683e83758d64abd89811dfad
Nikita Ershov <nikitadsk@mail.ru>
2019-08-19 16:52:57 +0300

    user-card add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MatToolbarModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatCardModule, MatIconModule } from '@angular/material';
 import { HeaderComponent } from './header/header.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
